039dbda713cd81f8f2f378c8234771df647c0e52
Sachin chaurasiya <mrsachinchaurasiya@gmail.com>
2024-06-02 16:49:47 +0530

    opencv

---
+++ JavaScript/First.js
@@ -0,0 +1,11 @@
+
+// Define variables
+let name = 'sachin';
+let age = 25;
+let isApproved = true;
+
+// Print values
+console.log("Hello, world!");
+console.log(name);
+console.log(age);
+console.log(isApproved);
