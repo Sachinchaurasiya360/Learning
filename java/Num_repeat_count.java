a7ffcb8f7887adb152878b15d524a73e5f1530a7
Sachin Chaurasiya <mrsachinchaurasiya@gmail.com>
2024-07-12 20:27:58 +0530

    zerodha structure

--- java/Num_repeat_count.java
+++ java/Num_repeat_count.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class Num_repeat_count {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-
+        int repeat=0;
         System.out.println("Enter your contact number");
         int contact= input.nextInt();
 
-        
+        while (contact>0){
+            if (contact %10==3){
+            repeat++;
+            }
+
+            contact /= 10;  // Without this step, the while loop will become an infinite loop because the value of contact never changes
+
+        }
+    System.out.println(repeat);
+
     }
 }
---
+++ java/reversenum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class reversenum {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your contact number");
+        int contact=input.nextInt();
+
+        int rev=0;
+        while (contact>0){
+            rev = rev * 10 + contact % 10;
+
+            contact/=10;
+
+        }
+        System.out.println(rev);
+    }
+}
